98fa2db1e0be6c6b5bba850a77b366591c0891d9
niuyang2579 <niuyang@mail.nwpu.edu.cn>
2019-04-28 11:35:01 +0800

    first commit

--- src/net/looperthread.h
+++ src/net/looperthread.h
@@ -1,6 +1,20 @@
-#ifndef LOOPERTHREAD_H
+#ifdef LOOPERTHREAD_H
 #define LOOPERTHREAD_H
 
-#include 
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+class Looper;
+
+class LooperThread
+{
+public:
+	LooperThread();
+	~LooperThread();
+
+	Looper* GetLoop();
+	
+};
 
 #endif // LOOPERTHREAD_H
